84faaa5b75b1ec86bcedc6e3f932cffc1c49add0
Rzki.hd <rzkihd8@gmail.com>
2023-10-23 13:48:07 +0700

    commit awal

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Hajimemashitte</h1>";
+    echo "nama saya kunkunmaru
+          saya tinggal di bogor
+          hobi saya ngegame";
+    }
+    
+}
